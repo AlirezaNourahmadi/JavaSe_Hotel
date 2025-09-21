ff1df8048ba5028470a9a23989965c306af9d668
alireza <alireza.noorahmadi67@gmail.com>
2025-09-21 19:12:51 +0330

    Entity implementations by Alireza Nourahmadi

---
+++ src/main/java/hotel/model/entity/Branch.java
@@ -0,0 +1,11 @@
+package hotel.model.entity;
+
+import java.util.List;
+
+public class Branch extends Hotel {
+    private int branchId;
+    private String address;
+    private List<Room> roomList;
+    private List<Employee> employeeList;
+
+}
---
+++ src/main/java/hotel/model/entity/Hotel.java
@@ -0,0 +1,8 @@
+package hotel.model.entity;
+
+import java.util.List;
+
+public class Hotel {
+    private String name;
+    private List<Branch> branchList;
+}
