903a9eb5980dd657ce60182dccc8726b55fd6482
Shayan Aminaei <shayanaminaei@gmail.com>
2025-09-23 21:10:08 +0330

    shayan aminaei - testformahdiyar  toziiihat

---
+++ src/main/java/hotel/model/entity/testformahdiyar.java
@@ -0,0 +1,17 @@
+package hotel.model.entity;
+
+public class testformahdiyar {
+    /*
+    *
+    *
+    * gdfhgfggdjgldj
+    * dgerwjhgoiwjghgoi
+    * ofigioidtjghijdgoihuj
+    * in ye test baraye mahdiyare.....
+    * badan pakesh mikonam
+    *
+    *
+    *
+    *
+    * */
+}
