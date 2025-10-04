ce29a8b2d0e97cf4735b156f8fc254613d89a226
alireza <alireza.noorahmadi67@gmail.com>
2025-10-04 16:25:17 +0330

    update by Alireza Nourahmadi

--- src/main/java/hotel/model/tools/BranchMapper.java
+++ src/main/java/hotel/model/tools/BranchMapper.java
@@ -1,5 +1,19 @@
 package hotel.model.tools;
 
+import hotel.model.entity.Branch;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class BranchMapper {
+    public Branch branchMapper(ResultSet resultSet) throws Exception {
+        return Branch
+                .builder()
+                .branchId(resultSet.getInt("branch_id"))
+                .address(resultSet.getString("address"))
+                .roomList(resultSet.getObject("room_list", ArrayList.class))
+                .employeeList(resultSet.getObject("employee_list", ArrayList.class))
+                .build();
+    }
     
 }
