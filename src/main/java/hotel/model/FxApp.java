58b6b8e0b3d75194340536060e5111b18897ce82
alireza <alireza.noorahmadi67@gmail.com>
2025-10-06 17:21:14 +0330

    debug by Alireza Nourahmadi

--- src/main/java/hotel/model/FxApp.java
+++ src/main/java/hotel/model/FxApp.java
@@ -1,4 +1,19 @@
 package hotel.model;
 
-public class FxApp {
+import hotel.model.tools.FormLoader;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class FxApp extends Application {
+    public static void main(String[] args) throws Exception{
+        log.info("Starting FxApp");
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FormLoader.getFormLoader().showStage(primaryStage, "/view/PersonView.fxml", "d_l");
+    }
 }
